a07c6e9de0efb8688165cc763cbe9832e9b7679a
MSprojects003 <mbaasatheef@gmail.com>
2025-07-07 03:51:28 +0530

    small iues changes

--- src/app/products/[category]/page.tsx
+++ src/app/products/[category]/page.tsx
@@ -1,18 +1,9 @@
-import ProductList from '@/components/custom/ProductList'
 import React from 'react'
-import { sampleProducts } from '../../data/products'
 
-type PageProps = { params: { category: string } }
-
-export default function Page({ params }: PageProps) {
-  // Format the category name for display
-  const displayCategory = params.category
-    .replace(/-/g, ' ')
-    .replace(/\b\w/g, l => l.toUpperCase())
-
+function page() {
   return (
-    <div className="max-w-7xl bg-gray-50 mx-auto px-4 sm:px-6 lg:px-8 py-8">
-      <ProductList products={sampleProducts} category={params.category} title={displayCategory} />
-    </div>
+    <div>page</div>
   )
 }
+
+export default page
